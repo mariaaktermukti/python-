80a9056e3ac909d25a9ddefa06286b0ec9f48cb6
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-05-01 10:10:30 +0600

    Add files via upload

---
+++ data_type.py
@@ -0,0 +1,32 @@
+   #data type
+        #1. interger-> +ve, -ve, 0
+        #2. string--> "Mukti" or 'mukti' or "'mukti'"
+        #3, float --> 2.56
+        #4. Boolean--> True, False (always capital letter)
+        #5. None--> a = None
+
+
+name = "Mukti"  
+age = 24
+price = 100.67
+
+#type
+print(type(name))     #str
+print(type(age))      #int
+print(type(price))    #float
+
+name1="mukti"
+name2='mukti'
+name3='''mukti'''
+
+print(name1)
+print(name2)
+print(name3)
+
+
+
+age2= 100
+old= False
+a= None
+print(type(old))
+print(type(a))
