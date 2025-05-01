2e9c37b523fa44a9146733f32cab03dfb4a0fc95
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-05-01 12:53:41 +0600

    Add files via upload

---
+++ input.py
@@ -0,0 +1,13 @@
+# name= input("Enter your name: ")
+# print("Welcome", name)
+
+# val= input("Enter some value :")
+# print(type(val), val)
+
+name= input("Enter your name: ")
+age=int(input("Enter your age: "))
+marks= float(input("Enter your marks : "))
+
+print("Welcome", name)
+print("age=", age)
+print("marks=", marks)
