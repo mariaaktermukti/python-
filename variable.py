549b0016192df2b0d7af0f1a0c550cf2e9a754ea
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-05-01 09:50:48 +0600

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,19 @@
+name = "Mukti"  #string 
+age = 24
+price = 100.67
+
+age2= age
+
+#print use variable's value--> don't use double Quotation Marks(" ")
+print(name)
+print(age)
+print(price)
+
+#or
+print("My name is :",name)
+print("My age is :", age2)
+print("The price is :", price)
+
+
+
+ 
