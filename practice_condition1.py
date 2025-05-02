c2b5ed7aac930bea6013ba30a8fbe8b65cac4af3
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-05-02 12:45:49 +0600

    Add files via upload

---
+++ practice_condition1.py
@@ -0,0 +1,11 @@
+light=input('Enter the light :')
+
+if(light == "red"):
+    print("Stop.!")  #indentation--> proper gap or spaces 
+elif(light == "green"):
+    print("Go.!") 
+elif(light== "yellow"):
+    print("Look.!") 
+    
+      
+print("End of code.")   
