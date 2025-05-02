70fc92bbc5a0c405f99076b6fdb3dfcc816a6e3e
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-05-02 10:27:46 +0600

    Add files via upload

---
+++ string_slicing.py
@@ -0,0 +1,15 @@
+str="My name is Mukti."
+print(str[0:9])
+print(str[3:34])
+print(str[0:0])
+print(str[5:len(str)])
+print(str[:9]) #[0:9]
+
+
+#negative index / backward counting
+# a    p    p    l    e
+# -5  -4   -3   -2   -1
+str1="apple"
+print(str1[-5:-2])  #app
+
+ 
