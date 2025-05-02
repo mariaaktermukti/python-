13f2a6e6939df83492352f2971977e35615b06ae
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-05-02 09:56:21 +0600

    Add files via upload

---
+++ string1.py
@@ -0,0 +1,6 @@
+str1="This is a string.\nWe are creating it in Python."  #all are valid-> double, single, anf triple quotation
+str2='My name is Mukti.'
+str3='''My age is 22.'''
+print(str1)
+print(str2)
+print(str3)
