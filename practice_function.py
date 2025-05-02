95e3224c0fe2a583434acab8de1567ba640e8f82
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-05-02 12:30:23 +0600

    Add files via upload

---
+++ practice_function.py
@@ -0,0 +1,11 @@
+# WAP to input user's first name and print it's length
+
+name= input("Enter your name : ")
+print("Length of your name is = ",len(name))
+
+
+#WAP to find the occurrence of '$' in a string
+
+str="Hi, $I am the $ symbol $99.9"
+print(str.count("$"))
+
