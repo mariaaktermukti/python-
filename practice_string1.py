72a99f7510f41cef0da2e68140a6817debd06274
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-05-03 10:07:33 +0600

    Add files via upload

---
+++ practice_string1.py
@@ -0,0 +1,11 @@
+movies=[]
+
+mov1=input("Enter 1st movie : ")
+mov2=input("Enter 2nd movie : ")
+mov3=input("Enter 3rd movie : ")
+
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+
+print(movies)
