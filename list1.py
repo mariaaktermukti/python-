8e3b590690776f5c8532bd310e1d138ff43c0b60
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-05-03 09:30:14 +0600

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,13 @@
+
+marks= [91.2,92.3,93.5,94.5,95.7,96.8,97.6,98.7,99.5,100.5]
+print(marks)
+print(type(marks))
+print(len(marks))
+print(marks[0])
+print(marks[9])
+
+         #ind:0     #1     #2
+student=["Mukti", 99, "Dhaka"]
+print(student)
+student[0]="Maria" 
+print(student)  #string is mutable 
