d1a28e01313bbc0aa42d9228277095dc339cbaac
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-05-03 09:54:17 +0600

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,11 @@
+# It is a built-in data type that lets us create immutable sequences of values
+tup=(9,1,3,4)
+print(type(tup))
+print(tup[0])  # 9
+print(tup[2])  # 3
+
+
+#empty tuple
+tup2=()
+print(tup2)
+print(type(tup2))
