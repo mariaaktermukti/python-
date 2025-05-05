7977edd935d36bfc38742d4e3310a55e6b9e79f9
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-05-05 16:45:54 +0600

    Add files via upload

---
+++ dictionary_function_values.py
@@ -0,0 +1,12 @@
+student={
+    "name":"Maria Mukti",
+    "Subjects":{   #nested
+        "phy": 96,
+        "chem": 98,
+        "math": 94,
+        "Bio": 99
+    }
+}
+
+print(student.values())  
+print(list(student.values()))
