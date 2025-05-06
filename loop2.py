803f30220670aa5fd2ded6b79f7802ef15aac91e
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-05-06 22:30:36 +0600

    Add files via upload

---
+++ loop2.py
@@ -0,0 +1,22 @@
+#print numbers from 1 to 5
+i=1
+while i<=5:
+    print(i)
+    i=i+1
+    
+print("Loop endes..") 
+
+
+#print numbers from 1 to 5 in reverse
+# j=5
+# while j>=1:
+#     print(j)  
+#     j=j-1
+    
+# print("Loop endes..")    
+# output:5
+# 4
+# 3
+# 2
+# 1
+# Loop endes..
