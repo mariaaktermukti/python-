7c75d5550e35d284f1d7eb29c289bfb76634bc44
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-05-06 21:40:36 +0600

    Add files via upload

---
+++ set_method_clear.py
@@ -0,0 +1,15 @@
+
+collection= set()
+collection.add(1)
+collection.add(2)
+collection.add(2) #ignore duplicate elements
+
+print(collection)  #{1, 2}
+# collection.remove(2)
+# print(collection) #{1}
+
+collection.add("Mukti")
+collection.add((1,2,3,4))
+collection.clear()
+print(collection) #set()
+print(len(collection)) #0
