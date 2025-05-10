049eb9d07a241a92649fc3f867f05b093c4b5403
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-05-10 18:31:35 +0600

    Add files via upload

---
+++ range.py
@@ -0,0 +1,28 @@
+# # Range functions returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and stops before a specified number.
+# # range( start?, stop, step?)
+# seq= range(5)
+# print(seq[0])
+# print(seq[1])
+# print(seq[2])
+# print(seq[3])
+# print(seq[4])
+
+# # or 
+# # for i in seq :
+# #     print(i)   bith show the same outout
+
+# # for i in range(10):  #range (stop)
+# #     print(i)
+    
+# for i in range(2,10):  #range (start, stop)
+#     print(i)    
+    
+for i in range(2,10,2):  #range (start, stop, step)
+    print(i)     
+    
+    
+    # output: 
+    #     2
+    #     4
+    #     6
+    #     8
