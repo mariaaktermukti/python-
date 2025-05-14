b5f4350a65949fe458d3ab6f5fd6d7497ef924c4
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-05-14 09:13:12 +0600

    Add files via upload

---
+++ practice_function1.py
@@ -0,0 +1,15 @@
+# WAF to print the length of a list. ( list is the parameter)
+
+cities=["Dhaka","mym","barisal"]
+heroes=["Thor","Iron man","Captain America"]
+
+def print_len(list):
+    print(len(list))
+
+print_len(cities)
+print_len(heroes)    
+
+
+# output: 
+#     3
+#     3
