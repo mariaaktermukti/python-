56ce2ebea430b755b1e74e75f1d9b2de2b48e335
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-05-14 09:02:11 +0600

    Add files via upload

---
+++ function1.py
@@ -0,0 +1,18 @@
+# def calculate_Sum(a,b):
+#     s=a+b
+#     return s
+# print(calculate_Sum(3,4))
+# print(calculate_Sum(10,10))
+# print(calculate_Sum(20,20))
+
+def print_hello():
+    print("Hello..!")
+
+print_hello()
+print_hello()
+#     output: 
+#         Hello..!
+#         Hello..!
+
+output= print_hello()
+print(output)   #None
