3f1205d149a358b30657e16eb8e3b593f9d7f78b
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-07-15 23:07:14 +0600

    Add files via upload

---
+++ escape_sequence.py
@@ -0,0 +1,7 @@
+# Hey Harry, please don't remove this line..
+print("I am a good girl.\nand my husband also a good boy.")
+
+"""I have a yt channel named honeyBun_13"""
+# or
+'''I have a yt channel named honeyBun_13'''
+print("Mukti", 6, 7, "Shifat")
